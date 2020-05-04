ef7448d22de1557be57789ac8d25c96d2d0c09fc
vutaka <vutaka0120@gmail.com>
2020-05-05 04:09:14 +0900

    スタイルをscriptで付けてみた

---
+++ change_style_in_script_performance/js/main.js
@@ -0,0 +1,63 @@
+function createRow(index) {
+  return '<tr><td>' + index + '</td><td>' + index + '行目</td></tr>';
+}
+
+/**
+ * 丹精込めてstyleを変更する
+ * @param {HTMLElement} table 
+ */
+function addStyle(table) {
+  const tds = table.querySelectorAll('td');
+  for (let td of tds) {
+    td.style.borderRight = 'solid 1px black';
+    td.style.borderBottom = 'solid 1px black';
+  }
+}
+/**
+ * 丹精込めてstyleを変更する。
+ * この場合animationframeでやるのはなんの意味もない。
+ * @param {HTMLElement} table 
+ */
+function addStyleInAnimationFrame(table) {
+  window.requestAnimationFrame(
+    function () {
+      addStyle(table);
+    }
+  );
+}
+/**
+ * テーブルになんかクラスをつける
+ * @param {HTMLElement} table 
+ */
+function addClass(table) {
+  table.setAttribute('class', 'border_on');
+}
+
+/**
+ * テーブルを初期化する
+ * @param {HTMLElement} table 
+ */
+function init(table) {
+  table.innerHTML = '';
+  table.setAttribute('class', '');
+  let rowStr = '';
+  for (let index = 0; index < 50000; index++) {
+    rowStr += createRow(index);
+  }
+  table.insertAdjacentHTML('beforeend', rowStr);
+}
+
+(function () {
+  const tableArea = document.getElementById("table");
+
+  const addStyleButton = document.getElementById("add_style");
+  const addStyleAnimationFrameButton = document.getElementById("add_style_animationframe");
+  const addClassButton = document.getElementById("add_class");
+  const initButton = document.getElementById("init");
+
+  addStyleButton.addEventListener('click', function () { addStyle(tableArea) });
+  addStyleAnimationFrameButton.addEventListener('click', function () { addStyleInAnimationFrame(tableArea) });
+  addClassButton.addEventListener('click', function () { addClass(tableArea) });
+  initButton.addEventListener('click', function () { init(tableArea) });
+  init(table);
+}());
